b5a24af17b4ee2cb4fbbba8eb44da333b7ba8173
Suleiman Leadbitter <suleiman.leadbitter@gmail.com>
2015-07-29 16:57:53 +0100

    Completed project and all working

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -4,7 +4,7 @@ describe TodoList do
   it { should have_many(:todo_items) }
 
   describe "#has_complete_items?" do
-  let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List")}
+    let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list") }
 
     it "returns true with completed todo list items" do
       todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
@@ -18,16 +18,17 @@ describe TodoList do
   end
 
   describe "#has_incomplete_items?" do
-    let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List")}
+    let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list") }
 
     it "returns true with incompleted todo list items" do
       todo_list.todo_items.create(content: "Eggs")
-      expect(todo_list.has_incompleted_items?).to be_true
+      expect(todo_list.has_incomplete_items?).to be_true
     end
 
-    it "returns false with no incompleted todo list items" do
+    it "returns false with no incomplete todo list items" do
       todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
-      expect(todo_list.has_incompleted_items?).to be_false
+      expect(todo_list.has_incomplete_items?).to be_false
     end
   end
+
 end
